053fb5a7d15b854af87f8707e28ff00ac2a5b6de
Askerov <askerovasif1993@mail.ru>
2025-11-11 23:52:41 +0300

    correct

--- test_task/main.py
+++ test_task/main.py
@@ -8,22 +8,24 @@ from indexing import indexing
 indexing(new_base)
 
 def main(base:str):
+    try:
+        with ObjectDataBaseConnect(base) as db:
+            old_val = db.select("""SELECT status, owner from data""", fetch_all=True)
 
-    with ObjectDataBaseConnect(base) as db:
-        old_val = db.select("""SELECT status, owner from data""", fetch_all=True)
+            row = select_one_doc(db)
 
-        row = select_one_doc(db)
+            doc_id, obj, operation_details = parsing_data(db, row)
 
-        doc_id, obj, operation_details = parsing_data(db, row)
+            all_parand_child = search_all_child(db, obj)
 
-        all_parand_child = search_all_child(db, obj)
+            correct_data(db, all_parand_child, operation_details)
 
-        correct_data(db, all_parand_child, operation_details)
-
-        set_processing_time(db, doc_id)
-        new_val = db.select("""SELECT status, owner from data""", fetch_all=True)
-        for k,v in zip(old_val, new_val):
-            print(k, k==v, v)
-        return True
+            set_processing_time(db, doc_id)
+            new_val = db.select("""SELECT status, owner from data""", fetch_all=True)
+            for k,v in zip(old_val, new_val):
+                print(k, k==v, v)
+            return True
+    except Exception as e:
+        return False
 
 print(main(new_base))
