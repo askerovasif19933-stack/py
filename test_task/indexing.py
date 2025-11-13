259c98e3ce530d2f3c743321f184f0dc0969c1d2
Askerov <askerovasif1993@mail.ru>
2025-11-13 11:32:28 +0300

    correct

--- test_task/indexing.py
+++ test_task/indexing.py
@@ -7,20 +7,21 @@ def indexing(base: str):
     """Индексация полей для быстрого доступа без блокировки таблиц (для больших таблиц)"""
     with ObjectDataBaseConnect(base) as db:
 
-        # Список индексов для создания
+        # Список индексов для создания 
+        # CREATE INDEX CONCURRENTLY - если таблица очень большая не блокирует запись 
         sql = [
             # Таблица documents
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_documents_processed_at ON documents (processed_at)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_documents_document_type ON documents (document_type)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_documents_recieved_at ON documents (recieved_at)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_documents_3_fields ON documents(processed_at, document_type, recieved_at)',
+            'CREATE INDEX IF NOT EXISTS idx_documents_processed_at ON documents (processed_at)',
+            'CREATE INDEX IF NOT EXISTS idx_documents_document_type ON documents (document_type)',
+            'CREATE INDEX IF NOT EXISTS idx_documents_recieved_at ON documents (recieved_at)',
+            'CREATE INDEX IF NOT EXISTS idx_documents_3_fields ON documents(processed_at, document_type, recieved_at)',
 
             # Таблица data
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_data_status ON data(status)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_data_parent ON data(parent)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_data_owner ON data(owner)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_data_object ON data(object)',
-            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_data_status_owner ON data(status, owner)'
+            'CREATE INDEX IF NOT EXISTS idx_data_status ON data(status)',
+            'CREATE INDEX IF NOT EXISTS idx_data_parent ON data(parent)',
+            'CREATE INDEX IF NOT EXISTS idx_data_owner ON data(owner)',
+            'CREATE INDEX IF NOT EXISTS idx_data_object ON data(object)',
+            'CREATE INDEX IF NOT EXISTS idx_data_status_owner ON data(status, owner)'
         ]
 
         # Выполняем каждый индекс отдельно
