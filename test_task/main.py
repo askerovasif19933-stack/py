6e98e5ca15bea9955a3c0e5cfd1c63cd8b4e472a
Askerov <askerovasif1993@mail.ru>
2025-11-15 02:21:39 +0300

    correct

--- test_task/main.py
+++ test_task/main.py
@@ -1,15 +1,13 @@
-
 from object_database_connect import ObjectDataBaseConnect
 from dal import select_one_doc, parsing_data, search_all_child, correct_data, set_processing_time, indexing, process_single_document 
 from create_base import new_base
-
 
 
         
 def main(base: str):
     try:
         with ObjectDataBaseConnect(base) as db:
-            old_val = db.select("""SELECT status, owner from data""", fetch_all=True)
+            # old_val = db.select("""SELECT status, owner from data""", fetch_all=True)
             indexing(db)
             while True:
                 
@@ -17,13 +15,12 @@ def main(base: str):
                 if not row:
                     print(f'Все документы обработаны')
                     break
-            new_val = db.select("""SELECT status, owner from data""", fetch_all=True)
-            for k,v in zip(old_val, new_val):
-                print(k, k==v, v)
+            # new_val = db.select("""SELECT status, owner from data""", fetch_all=True)
+            # for k,v in zip(old_val, new_val):
+            #     print(k, k==v, v)
     except Exception as e:
         return False
 
 
     return True
 
-print(main(new_base))
