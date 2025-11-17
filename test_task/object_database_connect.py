be28814e6e93ffa9aa066cb0abe8b639548f0fd1
Askerov <askerovasif1993@mail.ru>
2025-11-17 21:39:52 +0300

    correct class

--- test_task/object_database_connect.py
+++ test_task/object_database_connect.py
@@ -6,18 +6,16 @@ import psycopg2
 class ObjectDataBaseConnect:
 
     def __init__(self, base_name):
-
         self.connect_db= psycopg2.connect(
             user = user,
             host = host,
             port = port,
             password = password,
             database = base_name
-        )
-
-        
+        ) 
     
     def select(self, sql, parms=None, fetch_all=False):
+        """Выборка множесмтвеная или единичная"""
         with self.connect_db.cursor() as cur:
 
             cur.execute(sql, parms)
@@ -30,14 +28,10 @@ class ObjectDataBaseConnect:
         """Изменение и вставка данных"""
 
         with self.connect_db.cursor() as cur:
-            if ext_many:
+            if execute_many:
                 cur.executemany(sql, parms)
             else:
                 cur.execute(sql, parms)
-    
-    def close(self):
-        """Закрытие соединения"""
-        self.connect_db.close()
 
     def __enter__(self):
         return self
@@ -47,7 +41,7 @@ class ObjectDataBaseConnect:
             self.connect_db.rollback()
         else:
             self.connect_db.commit()
-        self.close()
+        self.connect_db.close()
 
         return False
 
